a346b7910345a485a6c01d27535a1a765d6c3411
Jeff Klein <jeff@jeffklein.org>
2015-04-27 08:00:48 -0700

    First stab at making a rest service for the UI. http://localhost:8080/codes/temp/all

--- src/main/java/org/jeffklein/turfwars/codes/backend/controller/TempCodeRestController.java
+++ src/main/java/org/jeffklein/turfwars/codes/backend/controller/TempCodeRestController.java
@@ -1,11 +1,16 @@
 package org.jeffklein.turfwars.codes.backend.controller;
 
+import org.jeffklein.turfwars.codes.dataaccess.model.TempCode;
+import org.jeffklein.turfwars.codes.dataaccess.service.TempCodeService;
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 /**
  * TODO: javadoc needed
@@ -14,11 +19,27 @@ import java.util.UUID;
  */
 @RestController
 public class TempCodeRestController {
-    @RequestMapping("/codes")
+
+    @Autowired
+    private TempCodeService tempCodeService;
+
+    @RequestMapping("/codes/temp/all")
     public Map<String, Object> home() {
         Map<String, Object> model = new HashMap<String, Object>();
-        model.put("id", UUID.randomUUID().toString());
-        model.put("content", "Hello World");
+        List<TempCode> tempCodes = tempCodeService.findAllTempCodes();
+        List codes = new ArrayList();
+        for (TempCode tempCode : tempCodes) {
+            Code code = new Code();
+            code.code = tempCode.getCode();
+            code.expirationDate = tempCode.getExpirationDate();
+            codes.add(code);
+        }
+        model.put("codes", codes);
         return model;
+    }
+
+    private static class Code {
+        public String code;
+        public DateTime expirationDate;
     }
 }
