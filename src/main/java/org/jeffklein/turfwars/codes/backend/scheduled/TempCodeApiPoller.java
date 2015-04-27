1a00830090b3f23fd47a765ac4fa7bf7ad0e7ecb
Jeff Klein <jeff@jeffklein.org>
2015-04-26 22:31:52 -0700

    Refactored to new api client and data access packages. now able to poll server and save codes to db

--- src/main/java/org/jeffklein/turfwars/codes/backend/scheduled/TempCodeApiPoller.java
+++ src/main/java/org/jeffklein/turfwars/codes/backend/scheduled/TempCodeApiPoller.java
@@ -1,10 +1,10 @@
 package org.jeffklein.turfwars.codes.backend.scheduled;
 
-import org.jeffklein.turfwars.codes.client.TempCodeApiJsonResponse;
+import org.jeffklein.turfwars.codes.client.TempCodeDTO;
 import org.jeffklein.turfwars.codes.client.TurfWarsApiClient;
 import org.jeffklein.turfwars.codes.dataaccess.model.TempCode;
-import org.jeffklein.turfwars.codes.dataaccess.model.TempCodeApiResponse;
 import org.jeffklein.turfwars.codes.dataaccess.service.TempCodeService;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -27,25 +27,22 @@ public class TempCodeApiPoller {
     @Scheduled(fixedRate = 20000)
     //TODO: this works exactly once then blows up with a Unique constraint violation. write a test case in the DAO module
     public void pollServerForTempCodes() {
-        TempCodeApiJsonResponse apiResponse = apiClient.getTempCodeApiJsonResponse();
-        TempCodeApiResponse toPersist = this.copyTempCodesFromJsonResponse(apiResponse.getTempCodes(), new TempCodeApiResponse());
-        toPersist.setTimestamp(apiResponse.getTimestamp());
-        toPersist.setNextUpdate(apiResponse.getNextUpdate());
-        tempCodeService.saveTempCodeApiResponse(toPersist);
+        Set<TempCodeDTO> apiResponse = apiClient.getTempCodes();
+        Set<TempCode> toPersist = this.copyTempCodesFromJsonResponse(apiResponse);
+        tempCodeService.saveTempCodeBatch(toPersist);
     }
 
     // TODO: copying fields one at a time is for the birds. need annotation based DTO copying here...
-    private TempCodeApiResponse copyTempCodesFromJsonResponse(Set<org.jeffklein.turfwars.codes.client.TempCode> jsonCodes,
-                                                        TempCodeApiResponse apiResponse) {
+    private Set<TempCode> copyTempCodesFromJsonResponse(Set<TempCodeDTO> jsonCodes) {
         Set<TempCode> dbCodes = new HashSet<TempCode>();
-        for (org.jeffklein.turfwars.codes.client.TempCode jsonCode : jsonCodes) {
+        for (TempCodeDTO jsonCode : jsonCodes) {
             TempCode dbCode = new TempCode();
-            dbCode.setCode(jsonCode.getCode());
-            dbCode.setExpires(jsonCode.getExpires());
-            dbCode.setTempCodeApiResponse(apiResponse);
+            dbCode.setCode(jsonCode.getTempCode());
+            dbCode.setExpirationDate(new DateTime(jsonCode.getExpirationDate()));
+            dbCode.setNextUpdateTimestamp(new DateTime(jsonCode.getServerNextBatchAvailableDate()));
+            dbCode.setServerTimestamp(new DateTime(jsonCode.getServerBatchPostedDate()));
             dbCodes.add(dbCode);
         }
-        apiResponse.setTempCodes(dbCodes);
-        return apiResponse;
+        return dbCodes;
     }
 }
